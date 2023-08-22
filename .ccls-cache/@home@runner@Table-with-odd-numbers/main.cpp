c07729134b18605e86f9e8faf19bd631666c8373
JacekPrtki <7378608-JacekPrtki@users.noreply.replit.com>
2023-08-22 16:56:25 +0000

    Wypełnienie tablicy wartościami nieparzystymi

--- .ccls-cache/@home@runner@Table-with-odd-numbers/main.cpp
+++ .ccls-cache/@home@runner@Table-with-odd-numbers/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+constexpr size_t tab_size = 100;
+
 int main() {
-  std::cout << "Hello World!\n";
+  int tab[tab_size];
+
+  for (size_t i = 0; i < tab_size; ++i) {
+    tab[i] = i;
+  }
+
+  for (size_t i = 0; i < tab_size; ++i) {
+    if (i % 2 == 1) {
+      std::cout << tab[i] << "\n";
+    }
+  }
+
+  return 0;
 }
